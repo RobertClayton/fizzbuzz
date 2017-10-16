937a7f9bc12f97d6058d519087833ac122813ecd
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-16 12:29:11 +0100

    Fizzbuzz project complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,40 @@
+
 def fizzbuzz(number)
-  'fizz'
+
+  #number.times do |counter|
+
+  if (number) % 3 == 0 && (number) % 5 ==0
+    return "FizzBuzz"
+  elsif (number) % 5 == 0
+    return "Buzz"
+  elsif (number) % 3 == 0
+    return "Fizz"
+  else
+    return number
+  end
+
+  #end
+
 end
+
+
+#fizzbuzz(20)
+
+
+=begin
+number.times do |counter|
+
+  if (counter+1) % 3 == 0 && (counter+1) % 5 ==0
+    puts "#{counter+1} --> FizzBuzz"
+  elsif (counter+1) % 5 == 0
+    puts "#{counter+1} --> Buzz"
+  elsif (counter+1) % 3 == 0
+    puts "#{counter+1} --> Fizz"
+  else
+    puts "#{counter+1} --> #{counter+1}"
+  end
+
+end
+
+#fizzbuzz(20)
+=end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "Fizz" when multiple of 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
+
+  it 'returns "Buzz" when multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+
+  it 'returns "FizzBuzz" when multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+
 end
